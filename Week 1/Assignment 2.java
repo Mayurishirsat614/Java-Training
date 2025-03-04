a43445a92729a3798f539cd237703c99a76fb322
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-03-04 10:40:19 +0530

    Update Assignment 2.java

--- Week 1/Assignment 2.java
+++ Week 1/Assignment 2.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
+
+public class CompoundInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter Principal Amount: ");
+        double principal = sc.nextDouble();
+        
+        System.out.print("Enter Rate of Interest: ");
+        double rate = sc.nextDouble();
+        
+        System.out.print("Enter Time (years): ");
+        int time = sc.nextInt();
+        
+        System.out.print("Enter number of times interest applied per year: ");
+        int n = sc.nextInt();
+        
+        double amount = principal * Math.pow(1 + (rate / 100) / n, n * time);
+        double interest = amount - principal;
+        
+        System.out.println("Compound Interest: " + interest);
+        sc.close();
+    }
+}
 
