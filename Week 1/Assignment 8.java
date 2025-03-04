0bb26ca5f0b28bcb3dc5e748b8dd5f0235f12239
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-03-04 10:49:52 +0530

    Update Assignment 8.java

--- Week 1/Assignment 8.java
+++ Week 1/Assignment 8.java
@@ -1,1 +1,19 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 1 and 100:");
+        for (int num = 1; num <= 100; num++) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
 
