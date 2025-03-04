c296ce2d550000a2c0ffc2b8ea1fb1fc4403ea34
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-03-04 10:48:15 +0530

    Update Assignment 7.java

--- Week 1/Assignment 7.java
+++ Week 1/Assignment 7.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+
+public class AsciiValue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+        
+        System.out.println("ASCII value of " + ch + " is: " + (int) ch);
+        
+        sc.close();
+    }
+}
 
