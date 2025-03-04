9b164642dd5161630ac1813af256a17fae9d59b4
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-03-04 10:47:40 +0530

    Update Assignment 6.java

--- Week 1/Assignment 6.java
+++ Week 1/Assignment 6.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
+
+class Circle {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+public class CircleMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+        
+        Circle c = new Circle(radius);
+        System.out.println("Area: " + c.getArea());
+        System.out.println("Circumference: " + c.getCircumference());
+        
+        sc.close();
+    }
+}
 
