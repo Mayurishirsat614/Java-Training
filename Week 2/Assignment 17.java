558f491782a463cd19d7717e02b4ed614045e4f7
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-03-16 20:14:21 +0530

    Update Assignment 17.java

--- Week 2/Assignment 17.java
+++ Week 2/Assignment 17.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+
+public class SquareRoot {
+    public static double squareRoot(int num) {
+        double temp, sqrt = num / 2.0;
+        do {
+            temp = sqrt;
+            sqrt = (temp + (num / temp)) / 2;
+        } while ((temp - sqrt) != 0);
+        return sqrt;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("Square Root: " + squareRoot(num));
+        sc.close();
+    }
+}
 
