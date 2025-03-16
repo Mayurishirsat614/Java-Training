698cc51ca748d6ea15bb1f710699337bc0fedc26
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-03-16 20:14:44 +0530

    Update Assignment 18.java

--- Week 2/Assignment 18.java
+++ Week 2/Assignment 18.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static boolean isArmstrong(int num) {
+        int sum = 0, original = num, digits = String.valueOf(num).length();
+        while (num > 0) {
+            sum += Math.pow(num % 10, digits);
+            num /= 10;
+        }
+        return sum == original;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("Is Armstrong: " + isArmstrong(num));
+        sc.close();
+    }
+}
 
