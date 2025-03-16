c0a415986d187f2f8fb9c57b53b4ecb0ab8af62f
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-03-16 20:10:55 +0530

    Update Assignment 13.java

--- Week 2/Assignment 13.java
+++ Week 2/Assignment 13.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static boolean isPalindrome(int num) {
+        int original = num, reversed = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        return original == reversed;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("Is Palindrome: " + isPalindrome(num));
+        sc.close();
+    }
+}
 
