5db57c6edb4b133f062b25166229be5d7ca4ecc0
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-03-16 20:15:37 +0530

    Update Assignment 20.java

--- Week 2/Assignment 20.java
+++ Week 2/Assignment 20.java
@@ -1,1 +1,13 @@
+public class AverageCalculator {
+    public static double average(int[] numbers) {
+        int sum = 0;
+        for (int num : numbers) sum += num;
+        return (double) sum / numbers.length;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("Average: " + average(numbers));
+    }
+}
 
