e890ec6e8aa9e5050d9016503f660c8065c71fbf
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 12:59:42 +0530

    Update Assignment6.java

--- Week 4and 5/Assignment6.java
+++ Week 4and 5/Assignment6.java
@@ -1,1 +1,13 @@
 
+import java.util.Scanner;
+
+public class HexagonalPrism {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base side length and height: ");
+        double a = sc.nextDouble(), h = sc.nextDouble();
+        double area = (6 * a * h) + (3 * Math.sqrt(3) * a * a);
+        System.out.println("Surface Area: " + area);
+        sc.close();
+    }
+}
