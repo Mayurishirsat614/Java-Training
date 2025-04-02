477a4e6463459b293ec80828613a9fc585e32718
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 16:46:14 +0530

    Update Assignment24.java

--- Week 2and3/Assignment24.java
+++ Week 2and3/Assignment24.java
@@ -1,1 +1,24 @@
 
+import java.util.Scanner;
+
+public class AddMatrices {
+    public static void main(String[] args) {
+        int[][] A = {{1, 2}, {3, 4}};
+        int[][] B = {{5, 6}, {7, 8}};
+        int[][] sum = new int[2][2];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                sum[i][j] = A[i][j] + B[i][j];
+            }
+        }
+
+        System.out.println("Sum of Matrices:");
+        for (int[] row : sum) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
