bff1e054af88ce56cc5fcb371f7d58b479ba09ac
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 12:46:55 +0530

    Update Assignment2.java

--- Week 4and 5/Assignment2.java
+++ Week 4and 5/Assignment2.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
+
+public class CGPACalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter CGPA: ");
+        double cgpa = sc.nextDouble();
+        double percentage = cgpa * 9.5;
+        System.out.println("Percentage: " + percentage + "%");
+        sc.close();
+    }
+}
 
