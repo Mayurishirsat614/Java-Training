7dc4856ad79ca8dfb5d3891b695b1275e766c17b
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 16:52:36 +0530

    Update Assignment40.java

--- Week 2and3/Assignment40.java
+++ Week 2and3/Assignment40.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+
+public class BinaryAddition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first binary string: ");
+        String a = sc.next();
+        System.out.print("Enter second binary string: ");
+        String b = sc.next();
+
+        int num1 = Integer.parseInt(a, 2);
+        int num2 = Integer.parseInt(b, 2);
+        int sum = num1 + num2;
+        String result = Integer.toBinaryString(sum);
+
+        System.out.println("Binary Sum: " + result);
+        sc.close();
+    }
+}
 
