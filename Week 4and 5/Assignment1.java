8352f2a56fa7fd49cd611e2e086cab99ce2b22c7
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 12:45:39 +0530

    Update Assignment1.java

--- Week 4and 5/Assignment1.java
+++ Week 4and 5/Assignment1.java
@@ -1,1 +1,10 @@
+public class OddPositionElements {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        System.out.println("Elements at odd positions:");
+        for (int i = 0; i < arr.length; i += 2) {
+            System.out.println(arr[i]);
+        }
+    }
+}
 
