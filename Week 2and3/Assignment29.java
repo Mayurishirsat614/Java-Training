4c033815d5e27755e668ca816cac327d2785bbb1
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 16:48:20 +0530

    Update Assignment29.java

--- Week 2and3/Assignment29.java
+++ Week 2and3/Assignment29.java
@@ -1,1 +1,17 @@
+public class RemoveAdjacentDuplicates {
+    public static void main(String[] args) {
+        String str = "aabbccddeeff";
+        StringBuilder sb = new StringBuilder();
+        char prev = '\0';
+
+        for (char c : str.toCharArray()) {
+            if (c != prev) {
+                sb.append(c);
+                prev = c;
+            }
+        }
+
+        System.out.println("String after removing duplicates: " + sb);
+    }
+}
 
