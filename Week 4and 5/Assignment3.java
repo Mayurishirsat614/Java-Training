881a98fbdc8c69cf521c9933fb56974be181bcf6
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 12:48:40 +0530

    Update Assignment3.java

--- Week 4and 5/Assignment3.java
+++ Week 4and 5/Assignment3.java
@@ -1,1 +1,15 @@
 
+import java.util.Scanner;
+
+public class DaysToYears {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of days: ");
+        int days = sc.nextInt();
+        int years = days / 365;
+        int weeks = (days % 365) / 7;
+        int remainingDays = (days % 365) % 7;
+        System.out.println(years + " years, " + weeks + " weeks, and " + remainingDays + " days.");
+        sc.close();
+    }
+}
