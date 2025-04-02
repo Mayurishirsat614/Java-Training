2e9b024d2119cb2adee1d7c9fb94044117f522e5
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 16:46:39 +0530

    Update Assignment25.java

--- Week 2and3/Assignment25.java
+++ Week 2and3/Assignment25.java
@@ -1,1 +1,10 @@
 
+import java.util.Arrays;
+
+public class SortStrings {
+    public static void main(String[] args) {
+        String[] words = {"Banana", "Apple", "Cherry", "Mango"};
+        Arrays.sort(words);
+        System.out.println("Sorted Strings: " + Arrays.toString(words));
+    }
+}
