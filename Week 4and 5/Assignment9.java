7df310cb4c7c04e5a85fa0e4db90e315c963a2c9
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 16:36:05 +0530

    Update Assignment9.java

--- Week 4and 5/Assignment9.java
+++ Week 4and 5/Assignment9.java
@@ -1,1 +1,15 @@
 
+import java.util.Scanner;
+
+public class EmployeeRating {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter salary and number of certifications: ");
+        double salary = sc.nextDouble();
+        int certs = sc.nextInt();
+        double increment = certs >= 10 ? 0.10 : certs >= 5 ? 0.05 : certs >= 1 ? 0.025 : 0;
+        salary += salary * increment;
+        System.out.println("Updated Salary: " + salary);
+        sc.close();
+    }
+}
