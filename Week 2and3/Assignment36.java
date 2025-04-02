c154b683cced53e1d1a2f8b36bba0e597ac6dd35
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 16:51:04 +0530

    Update Assignment36.java

--- Week 2and3/Assignment36.java
+++ Week 2and3/Assignment36.java
@@ -1,1 +1,23 @@
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class UniqueCharacters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        Set<Character> uniqueChars = new LinkedHashSet<>();
+
+        for (char ch : input.toCharArray()) {
+            uniqueChars.add(ch);
+        }
+
+        System.out.print("Unique characters: ");
+        for (char ch : uniqueChars) {
+            System.out.print(ch + " ");
+        }
+        sc.close();
+    }
+}
 
