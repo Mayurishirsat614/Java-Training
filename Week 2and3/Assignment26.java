e3b65a16d6d1db3a944737fb67dbc04f33a1f60a
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 16:47:12 +0530

    Update Assignment26.java

--- Week 2and3/Assignment26.java
+++ Week 2and3/Assignment26.java
@@ -1,1 +1,19 @@
 
+public class HighestNumbers {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 4, 45, 99, 77};
+        int highest = Integer.MIN_VALUE, secondHighest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > highest) {
+                secondHighest = highest;
+                highest = num;
+            } else if (num > secondHighest && num != highest) {
+                secondHighest = num;
+            }
+        }
+
+        System.out.println("Highest: " + highest);
+        System.out.println("Second Highest: " + secondHighest);
+    }
+}
