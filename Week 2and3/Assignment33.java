cb755bcc870c4ef70f022a9e6f1f7a9f064cb7c2
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 16:49:49 +0530

    Update Assignment33.java

--- Week 2and3/Assignment33.java
+++ Week 2and3/Assignment33.java
@@ -1,1 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        List<Integer> evenNumbers = new ArrayList<>();
+
+        for (int num : arr) {
+            if (num % 2 == 0) evenNumbers.add(num);
+        }
+
+        System.out.println("Even Numbers: " + evenNumbers);
+    }
+}
 
