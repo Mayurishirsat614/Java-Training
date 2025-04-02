f8a2d129ce56b076e9d8da90cab3d9dc86846155
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 16:51:24 +0530

    Update Assignment37.java

--- Week 2and3/Assignment37.java
+++ Week 2and3/Assignment37.java
@@ -1,1 +1,12 @@
+public class StringImmutable {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println("Original String: " + str);
+        str.concat(" World");  // This won't modify str
+        System.out.println("After concat: " + str);  // Proves immutability
+
+        str = str.concat(" World"); // New object created
+        System.out.println("New String: " + str);
+    }
+}
 
