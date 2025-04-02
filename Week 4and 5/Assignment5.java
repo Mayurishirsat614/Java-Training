406a3a5893d669617c7898b2d46d7667e6ed0ee0
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 12:57:15 +0530

    Update Assignment5.java

--- Week 4and 5/Assignment5.java
+++ Week 4and 5/Assignment5.java
@@ -1,1 +1,13 @@
 
+import java.util.Scanner;
+
+public class RectangularPrism {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length, width, and height: ");
+        double l = sc.nextDouble(), w = sc.nextDouble(), h = sc.nextDouble();
+        double area = 2 * (l * w + w * h + l * h);
+        System.out.println("Surface Area: " + area);
+        sc.close();
+    }
+}
