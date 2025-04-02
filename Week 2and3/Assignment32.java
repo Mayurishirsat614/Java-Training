5272b0c5add12c6106183e21526c53ab714ef479
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 16:49:30 +0530

    Update Assignment32.java

--- Week 2and3/Assignment32.java
+++ Week 2and3/Assignment32.java
@@ -1,1 +1,8 @@
+public class ReplaceDigits {
+    public static void main(String[] args) {
+        String str = "H3ll0W0rld";
+        String replaced = str.replaceAll("[0-9]", "*");
+        System.out.println("Modified String: " + replaced);
+    }
+}
 
