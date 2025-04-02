bf99e5180808ca811817400d469ce58c8eaefeeb
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 16:48:42 +0530

    Update Assignment30.java

--- Week 2and3/Assignment30.java
+++ Week 2and3/Assignment30.java
@@ -1,1 +1,23 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FirstUniqueCharacter {
+    public static void main(String[] args) {
+        String str = "swiss";
+        Map<Character, Integer> countMap = new LinkedHashMap<>();
+
+        for (char ch : str.toCharArray()) {
+            countMap.put(ch, countMap.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : countMap.keySet()) {
+            if (countMap.get(ch) == 1) {
+                System.out.println("First Unique Character: " + ch);
+                return;
+            }
+        }
+
+        System.out.println("No unique character found.");
+    }
+}
 
