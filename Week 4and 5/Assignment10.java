b637ab390aec136290cdfc0fa5d367477291ee06
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 16:36:33 +0530

    Update Assignment10.java

--- Week 4and 5/Assignment10.java
+++ Week 4and 5/Assignment10.java
@@ -1,1 +1,15 @@
 
+import java.util.Scanner;
+
+public class VehicleLoan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter principal, rate (%), time (years): ");
+        double P = sc.nextDouble(), R = sc.nextDouble(), T = sc.nextDouble();
+        R = R / (12 * 100);
+        T = T * 12;
+        double EMI = (P * R * Math.pow(1 + R, T)) / (Math.pow(1 + R, T) - 1);
+        System.out.println("EMI: " + EMI);
+        sc.close();
+    }
+}
