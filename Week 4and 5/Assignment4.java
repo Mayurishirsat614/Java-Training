1f55267751bba31a8d179434257bf8270b755499
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 12:51:37 +0530

    Update Assignment4.java

--- Week 4and 5/Assignment4.java
+++ Week 4and 5/Assignment4.java
@@ -1,1 +1,20 @@
 
+public class DisariumNumbers {
+    static boolean isDisarium(int num) {
+        int sum = 0, temp = num, length = String.valueOf(num).length();
+        while (temp > 0) {
+            sum += Math.pow(temp % 10, length--);
+            temp /= 10;
+        }
+        return sum == num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Disarium numbers between 1 and 100:");
+        for (int i = 1; i <= 100; i++) {
+            if (isDisarium(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
