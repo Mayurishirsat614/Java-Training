af010250b765762441cbdafedadcd03cddbf7c01
Mayurishirsat614 <158141490+Mayurishirsat614@users.noreply.github.com>
2025-04-02 13:01:52 +0530

    Update Assignment7.java

--- Week 4and 5/Assignment7.java
+++ Week 4and 5/Assignment7.java
@@ -1,1 +1,13 @@
 
+import java.util.Scanner;
+
+public class WindSpeed {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter temperature (°C) and wind speed (km/h): ");
+        double t = sc.nextDouble(), v = sc.nextDouble();
+        double windChill = 13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16);
+        System.out.println("Wind Chill: " + windChill);
+        sc.close();
+    }
+}
